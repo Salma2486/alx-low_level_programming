37923f2eb29094d25f0b44ad5a673a4d178dff12
salma <saloom_001@hotmail.com>
2023-08-10 09:08:09 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *
+ *Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
