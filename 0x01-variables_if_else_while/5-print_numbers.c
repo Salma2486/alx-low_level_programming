9f13bc5867034e4e7459d68ebbff6553729a1005
salma <saloom_001@hotmail.com>
2023-08-11 08:54:16 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *
+ *Return:0 Success
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		printf(a);
+	}
+	return (0);
+}
