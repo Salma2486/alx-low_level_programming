edd776d4f0fd0debea312d6628860e81c8c0f9c1
salma <saloom_001@hotmail.com>
2023-08-11 12:12:13 -0700

    task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *
+ *Return:0 Success
+ */
+int main(void)
+{
+	int n = 0, m;
+
+	while (n <= 99)
+	{
+		m = n
+			while (m <= 99)
+			{
+				if (m != n)
+				{
+					putchar((n / 10) + 48);
+					putchar((n % 10) + 48);
+					putchar(' ');
+					putchar((m / 10) + 48);
+					putchar((m % 10) + 48);
+					if (n != 98 || m != 99)
+					{
+						putchar(',');
+						putchat(' ');
+					}
+				}
+				m++;
+			}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
