4098cf211a9f08615cc6fa93c16450b9ecbd05b7
salma <saloom_001@hotmail.com>
2023-08-11 11:49:42 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *
+ *Return:0 Success
+ */
+int main(void)
+{
+	int = 0, b;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a + 48);
+				putchar(b + 48);
+				if (a + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
