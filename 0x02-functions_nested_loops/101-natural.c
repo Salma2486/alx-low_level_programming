399eecd8bd986c21e55c9cc05cbdd9b83c6caf1d
salma <saloom_001@hotmail.com>
2023-08-15 13:39:43 -0700

    task13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - This is the entry point of the code
+ *
+ *Return:0 Success
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if (((num % 3 == 0) || (num % 5 == 0))
+				sum += num;
+	}
+	printf("%d\n", num);
+	return (0);
+}
