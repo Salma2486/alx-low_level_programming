a3dde51a890aa2ee3a366c95104cd69591a3786c
salma <saloom_001@hotmail.com>
2023-08-15 09:54:31 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,12 +9,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar(43);
 	return (1);
+	}
 	if (n == 0)
+	{
 		_putchar(48);
 	return (0);
-	if (n < 0)
+	}
+	else if (n < 0)
+	{
 		_putchar(45);
 	return (-1);
+	}
 }
