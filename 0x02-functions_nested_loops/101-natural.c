e690fca486bdf6120241c83ffe6a974aa4bef05e
salma <saloom_001@hotmail.com>
2023-08-15 13:43:10 -0700

    task13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (num = 0; num < 1024; num++)
 	{
-		if (((num % 3 == 0) || (num % 5 == 0))
+		if ((num % 3 == 0) || (num % 5 == 0))
 				sum += num;
 	}
 	printf("%d\n", num);
