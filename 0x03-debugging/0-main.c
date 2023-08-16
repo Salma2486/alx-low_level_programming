6520a61190d55b87765f70ace69b565215272d5a
salma <saloom_001@hotmail.com>
2023-08-16 07:01:32 -0700

    task0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,36 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- *main - This is the entry point of the code
- *
- *Description: 'the program's description'
- *
- *Return:0 Success
- */
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-
-	return (0);
-}
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -39,7 +7,7 @@ int main(void)
 {
 	int i;
 
-	i % 2;
+	i  = 0;
 	positive_or_negative(i);
 	return (0);
 }
