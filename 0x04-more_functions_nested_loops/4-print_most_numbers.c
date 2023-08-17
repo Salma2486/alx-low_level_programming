55db2a2b0429ec1d6fca2259466e2206a79d15f8
salma <saloom_001@hotmail.com>
2023-08-17 04:45:40 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_most_numbers - This is the entry point of the code
+ */
+void print_most_numbers(void)
+{
+	char ch = 48;
+
+	while (ch >= 48 && ch <= 57)
+	{
+		while (ch == 50 || ch ==52)
+		{
+			ch++;
+		}
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
+
