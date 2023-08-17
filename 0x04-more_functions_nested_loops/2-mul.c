37f1218bfe4b43d52dfbf176ecd3e10c782098e9
salma <saloom_001@hotmail.com>
2023-08-17 04:25:58 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *mul - This is the entry point of the code
+ *
+ *@a: first input
+ *@b: second input
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
