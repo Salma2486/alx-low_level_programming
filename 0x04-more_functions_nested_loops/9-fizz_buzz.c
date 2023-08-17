f92203c55bf3653c7cd3e002a3d3db49a3d0d05f
salma <saloom_001@hotmail.com>
2023-08-17 07:14:56 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@ int main(void)
 
 	for (a = 0; a <= 100; a++)
 	{
-		if (a % 3 == 0)
+		if (a % 3 == 0 && a % 5 != 0)
 			printf("Fizz");
-		else if (a % 5 == 0)
+		else if (a % 5 == 0 && a % 3 != 0)
 			printf("Buzz");
 		else if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz");
