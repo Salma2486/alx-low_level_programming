d268ec4a0f9ac18e3623b41c82d5c952b052339d
salma <saloom_001@hotmail.com>
2023-08-17 10:11:15 -0700

    task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_triangle - This is the entry point of the code
+ *
+ *@size: input
+ */
+void print_triangle(int size)
+{
+	int a, b, space;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (space = a + 1; space < size; space++)
+			{
+				_putchar(' ');
+			}
+			for (b = 0; b <= a; b++)
+					{
+					_putchar('#');
+					}
+		}
+		_putchar('\n');
+	}
+}
