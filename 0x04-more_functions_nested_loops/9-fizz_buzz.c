410dd5943411295249c96722156edea47768c6ee
salma <saloom_001@hotmail.com>
2023-08-17 07:09:55 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *
+ *Return:0 Success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("Fizz ");
+		else if (a % 5 == 0)
+			printf("Buzz ");
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+
+	return (0);
+}
