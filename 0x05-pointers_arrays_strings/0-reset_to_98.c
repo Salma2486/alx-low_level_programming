3170cb687cd717b535381a0e9a6aaa41018953cf
salma <saloom_001@hotmail.com>
2023-08-21 07:29:21 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - This is the entry point of the code
+ *
+ *@n: input
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers first readme file
