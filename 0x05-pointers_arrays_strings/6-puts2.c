03a9c765da7ae02aa3462f04f07d63f0a66a5a73
salma <saloom_001@hotmail.com>
2023-08-21 11:56:15 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *puts2 - This is the entry point of the code
+ *
+ *@str: input
+ */
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a += 2)
+		_putchar(str[a]);
+	_putchar('\n');
+}
