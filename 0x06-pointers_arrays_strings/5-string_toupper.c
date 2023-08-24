3f4499699828f7233ee9cceb9f2704e01890ee3a
salma <saloom_001@hotmail.com>
2023-08-24 08:55:36 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ **string_toupper - This is the entry point of the code
+ *
+ *@str: input
+ *Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
