8c3d0a3a34a4db4343e0ed8d2957352553c20707
salma <saloom_001@hotmail.com>
2023-08-24 06:52:35 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ **_strncpy - This is the entry point of the code
+ *
+ *@n: input 3
+ *@dest: input 1
+ *@src: input 2
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
