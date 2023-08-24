116c0ee2ed438f397d6eaf6f428fce685ef42971
salma <saloom_001@hotmail.com>
2023-08-24 10:49:05 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -27,8 +27,10 @@ char *cap_string(char *str)
 			str[index - 1] == '{' ||
 			str[index - 1] == '}' ||
 			index == 0)
+			{
 				str[index] -= 32;
-			index++;
+				index++;
+			}
 			}
 	return (str);
 }
