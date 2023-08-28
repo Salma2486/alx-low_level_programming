aeeaf8a2c353c66bba65ce51daf8b456cdcb5256
salma <saloom_001@hotmail.com>
2023-08-28 08:27:29 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ **_strchr - This is the entry point of the code
+ *@s: input 1
+ *@c: input 2
+ *
+ *Return:0 Success
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
