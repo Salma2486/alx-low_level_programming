bae01f6a65bb5e646a7daa262864a6ebd1261073
salma <saloom_001@hotmail.com>
2023-08-28 13:10:13 -0700

    task6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_chessboard - This is the entry point of the code
+ *
+ *@a: input
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
+}
