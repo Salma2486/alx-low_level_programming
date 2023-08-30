a76bd0e28e4c9c00bbdfcaccfbcb0b53543f333e
salma <saloom_001@hotmail.com>
2023-08-30 13:40:11 -0700

    task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_pow_recursion - This is the entry point of the code
+ *
+ *@x: input 1
+ *@y input 2
+ *Return:0 Success
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	{
+		return (x *=  _pow_recursion(x, (y - 1)));
+	}
+}
