cfbd82752981c698595c8a45f54f6cae495c7c5c
salma <saloom_001@hotmail.com>
2023-08-30 15:58:54 -0700

    task8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *wildcmp - This is the entry point of the code
+ *@s1: input 1
+ *@s2: input 2
+ *Return:0 Success
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 +1));
+		}
+			return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
