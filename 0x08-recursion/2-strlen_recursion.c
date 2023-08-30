07962501dca26b812ab833d6425e2c809400fdf8
salma <saloom_001@hotmail.com>
2023-08-30 12:40:54 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_strlen_recursion - This is the entry point of the code
+ *@s: input
+ *Return:0 Success
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return(0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
