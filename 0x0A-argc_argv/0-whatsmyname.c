4c4bc2a4d683bde9b46c99ba1c0eed8778e9f6b6
salma <saloom_001@hotmail.com>
2023-09-01 12:29:20 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *@argc: input 1
+ *@argv: input 2
+ *
+ *Return:0 Success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
