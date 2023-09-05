c1f5c4a2a65dda7222d6b270f59e8721392d29eb
salma <saloom_001@hotmail.com>
2023-09-05 07:15:23 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  **_strdup - This is the entry point of the code
  *@str: input
  *
  *Return:0 Success
  */
-har *_strdup(char *str)
+char *_strdup(char *str)
+{
+	char *r;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	r = malloc((strlen(str + 1)) * sizeof(char));
+	if (r == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= strlen(str); i++)
+	{
+		r[i] = str[i];
+	}
+	return (r);
+}
