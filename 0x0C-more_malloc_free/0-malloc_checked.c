3eb7a1e370b957731cd9e7e346d76cee6b0b7e34
salma <saloom_001@hotmail.com>
2023-09-07 07:14:13 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ **malloc_checked - This is the entry point of the code
+ *@b: input
+ *Return:0 Success
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
