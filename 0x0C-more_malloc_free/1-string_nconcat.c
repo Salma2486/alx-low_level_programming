b33e04a8bbe8fb327a0ff2cf1c36e090b60230a1
salma <saloom_001@hotmail.com>
2023-09-07 10:36:43 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,30 +11,23 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
-	unsigned int len1;
-	unsigned int len2;
 	char *ptr;
 
 	if (s1 == NULL)
 	{s1 = ""; }
 	if (s2 == NULL)
 	{s2 = ""; }
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
-	ptr = malloc(len1 + n + 1);
-
+	ptr = malloc(strlen(s1) + n + 1);
 	if (ptr == NULL)
 	{return (NULL); }
-	for (i = 0; i < len1; i++)
+	for (i = 0; i < strlen(s1); i++)
 	 {ptr[i] = s1[i]; }
-	if (n >= len2)
-		n = len2;
+	if (n >= strlen(s2))
+		n = strlen(s2);
 	for (i = 0; i < n; i++)
 	{
-		ptr[len1 + i] = s2[i];
+		ptr[strlen(s1) + i] = s2[i];
 	}
-	ptr[i + len1] = '\0';
+	ptr[i + strlen(s1)] = '\0';
 	return (ptr);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ **_calloc - This is the entry point of the code
+ *@nmemb: input 1
+ *@size: input 2
+ *Return:0 Success
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
