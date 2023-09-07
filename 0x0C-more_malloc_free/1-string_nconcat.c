423f47d1e3015281d89ada702c87dd2a1d8cb3ed
salma <saloom_001@hotmail.com>
2023-09-07 08:09:39 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,18 +14,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 	if (n >= strlen(s2))
+	{
 		n = strlen(s2);
-
+	}
 	for (i = 0; i < strlen(s1); i++)
+	{
 		ptr[i] = s1[i];
+	}
 	for (i = 0; i < n; i++)
+	{
 		ptr[i + strlen(s1)] = s2[i];
+	}
 	return (ptr);
 
 }
