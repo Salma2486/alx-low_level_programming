6b0040216b2cc4cedd3c5620a7d36294c423ab12
salma <saloom_001@hotmail.com>
2023-09-11 06:54:51 -0700

    task2

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point of the code
+ *
+ *Return:0 Success
+ */
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
