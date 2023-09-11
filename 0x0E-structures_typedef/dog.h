c6b54ff7c3e97f12867e6a3fc0b2ab3423a5e649
salma <saloom_001@hotmail.com>
2023-09-11 13:00:29 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
-	char* name;
+/**
+ *struct dog - This is the entry point of the code
+ *@name: input 1
+ *@age: input 2
+ *@owner: input 3
+ *Return:0 Success
+ */
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 #endif
