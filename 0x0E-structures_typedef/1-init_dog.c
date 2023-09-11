0ade65b8bd622be23387b2f48e2cc313d1c9d7da
salma <saloom_001@hotmail.com>
2023-09-11 13:50:10 -0700

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ *init_dog - This is the entry point of the code
+ *@d: input 1
+ *@name: input 2
+ *@age: input 3
+ *@owner: input 4
+ *Return:0 Success
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
