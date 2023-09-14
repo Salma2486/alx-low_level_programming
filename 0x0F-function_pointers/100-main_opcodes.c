fcef12beee385698cb3290c83951ce58cdd727e0
salma <saloom_001@hotmail.com>
2023-09-13 17:17:08 -0700

    task4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - This is the entry point of the code
+ *@argc: kjsdf
+ *@argv: ushdfovno
+ *
+ *Return:0 Success
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, nbytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
