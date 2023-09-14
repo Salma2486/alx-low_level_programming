5885a017904445d07850d92c3be70dfbe939e5f7
salma <saloom_001@hotmail.com>
2023-09-13 17:08:33 -0700

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - This is the entry point of the code
+ *@array: input
+ *@size_t: i
+ *@action: iusfw
+ *Return:0 Success
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
