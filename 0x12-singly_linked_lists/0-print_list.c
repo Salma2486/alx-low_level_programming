ac1d520c1761bc130d2dfa955c958437d35353a9
Salma2486 <saloom_001@hotmail.com>
2023-09-21 14:52:25 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		i++;
 	}
-	return (s);
+	return (i);
 }
