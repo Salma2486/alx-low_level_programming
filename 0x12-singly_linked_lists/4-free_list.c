7bebe29d94891ed9da3309050dd698bb01bb526b
Salma2486 <saloom_001@hotmail.com>
2023-09-23 12:20:46 -0700

    task4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 /**
  *free_list - This is the entry point of the code
- *@head: kjfkreu
+ *@head: fjegvdnfjo
  *Return:0 Success
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *temp = head;
 
 	while (temp != NULL)
 	{
 		temp = temp->next;
+		free(head->str);
 		free(head);
 		head = temp;
 	}
