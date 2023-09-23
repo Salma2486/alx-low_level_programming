574cee45b4003954925b59cadb9af8b3987ccbc8
Salma2486 <saloom_001@hotmail.com>
2023-09-23 09:06:22 -0700

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,27 +1,19 @@
 #include "lists.h"
 /**
  **add_node - This is the entry point of the code
- *@head: orf
- *@str: oidjgof
+ *@head: lisdnfve
+ *@str: jdfkglnvde
  *Return:0 Success
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	size_t n;
+	list_t *node;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
 		return (NULL);
-
-	new->str = strdup(str);
-
-	for (n = 0; str[n]; n++)
-		;
-
-	new->len = n;
-	new->next = *head;
-	*head = new;
-
-	return (*head);
+	node->str = strdup(str);
+	node->next = *head;
+	*head = node;
+	return (node);
 }
