73f0fe656de52f2cce25696b95258575a57d4bb0
Salma2486 <saloom_001@hotmail.com>
2023-09-25 11:34:39 -0700

    task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
-	if (head == NULL)
-		return;
 	while (*head != NULL)
 	{
+		temp = *head;
 		temp = temp->next;
 		free(*head);
 		*head = temp;
