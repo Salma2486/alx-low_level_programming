b920676308d537883e25cb7f19587906837ce506
Salma2486 <saloom_001@hotmail.com>
2023-09-25 16:26:56 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,11 +23,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = ptr;
 		return (ptr);
 	}
-	for (i = 0; i < idx - 1; i++)
+	else
 	{
-		ptr = ptr->next;
+		for (i = 0; i < idx - 1; i++)
+		{
+			ptr = ptr->next;
+		}
+		ptr2->next = ptr->next;
+		ptr->next = ptr2;
 	}
-	ptr2->next = ptr->next;
-	ptr->next = ptr2;
 	return (ptr2);
 }
