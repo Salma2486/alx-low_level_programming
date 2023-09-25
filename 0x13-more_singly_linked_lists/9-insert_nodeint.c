59a92bbd63b27f22d0cd26611b2c2be622b2ee11
Salma2486 <saloom_001@hotmail.com>
2023-09-25 16:52:13 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,10 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		ptr2->next = ptr;
 		ptr = ptr2;
 	}
-	else
-	{
-		ptr2->next = ptr->next;
-		ptr->next = ptr2;
-	}
+	ptr2->next = ptr->next;
+	ptr->next = ptr2;
 	return (ptr2);
 }
