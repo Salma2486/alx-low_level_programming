cf19e8b014eb0566bd1b7b35ae1d09ca716cf24a
Salma2486 <saloom_001@hotmail.com>
2023-09-26 13:19:33 -0700

    task102

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,32 @@
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ptr;
+	listint_t *p;
+
+	ptr = head;
+	p = head;
+	while (head && ptr && ptr->next)
+	{
+		head = head->next;
+		ptr = ptr->next->next;
+		if (head == ptr)
+		{
+			head = p;
+			p =  p2;
+			while (1)
+			{
+				ptr = p;
+				while (ptr->next != head && ptr->next != p)
+				{
+					ptr = ptr->next;
+				}
+				if (ptr->next == head)
+					break;
+
+				head = head->next;
+			}
+			return (ptr->next);
+		}
+	}
+	return (NULL);
+}
