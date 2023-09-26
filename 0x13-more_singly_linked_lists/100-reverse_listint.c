cf9f9224908872316121827ba141229f6a6fcb34
Salma2486 <saloom_001@hotmail.com>
2023-09-26 12:39:17 -0700

    osdfgrwr

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,3 +1,9 @@
+#include "lists.h"
+/**
+ **reverse_listint - This is the entry point of the code
+ *@head: juei
+ *Return:0 Success
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *i, *j;
