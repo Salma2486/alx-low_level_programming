cca7b4f9e3b763e2956da288354fbd37ec383b38
Salma2486 <saloom_001@hotmail.com>
2023-09-28 15:54:23 -0700

    task3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *set_bit - This is the entry point of the code
+ *@n: jksfungvikrf
+ *@index: ikjodfgnevioe
+ *Return:0 Success
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index >= 64)
+	{
+		return (-1);
+	}
+	i = 1 << index;
+	return (*n | i);
+
+}
