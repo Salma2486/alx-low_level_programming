83baa00bafde8565437841e3a847eee0f146b9ec
Salma2486 <saloom_001@hotmail.com>
2023-11-13 02:26:42 -0800

    task2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 int add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 int sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 int mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 int div(int a, int b)
 {
-    if (b == 0)
-    {
-        return (0);
-    }
-    return (a / b);
+	if (b == 0)
+	{
+		printf("Error");
+		return (0);
+	}
+	return (a / b);
 }
 int mod(int a, int b)
 {
-    if (b == 0)
-    {
-        return (0);
-    }
-    return (a % b);
+	if (b == 0)
+	{
+		printf("Error");
+		return (0);
+	}
+	return (a % b);
 }
