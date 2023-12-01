0f6bd67487975be729df52c8503823358fe63311
Salma2486 <saloom_001@hotmail.com>
2023-12-01 10:57:43 -0800

    task100

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -81,6 +81,5 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
 #endif
 
