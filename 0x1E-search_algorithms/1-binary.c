dfc14e7d98963591c0b0f042d3b48a71f090cdbe
Salma2486 <saloom_001@hotmail.com>
2024-03-11 23:48:11 +0000

    task11

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,26 @@
 #include "search_algos.h"
 /**
+ *print_array - This is the entry point of the code
+ *@array: srdg srt g
+ *@start: rtsh grt h
+ *@end: rgsdg srtg k
+ *Return:0 Success
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		printf("%d", array[i]);
+		if (i < end)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
+/**
  *binary_search - rlktsjgnuljk iowhr
  *@array: rtfh ff - This is the entry point of the code
  *@size: trf hgsrtf
@@ -14,6 +35,7 @@ int binary_search(int *array, size_t size, int value)
 		return (-1);
 	while (l <= h)
 	{
+		print_array(array, l, h);
 		m = (l + h) / 2;
 		if (array[m] == value)
 			return (m);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,8 +6,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
-
-
+void print_array(int *array, size_t start, size_t end);
 #endif
 
